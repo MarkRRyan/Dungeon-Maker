71a30915b8034d3bc14b991da3f077982a32ed46
MarkRRyan <markrryan99@gmail.com>
2024-07-29 19:36:25 -0700

    added random goblin generation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('This is a test')
+document.getElementById('getGoblins').addEventListener('click', function() {
+    const items = ['1 Goblin', '2 Goblins', '3 Goblins', '4 Goblins', '5 Goblins']
+    const goblinAmount = items[Math.floor(Math.random() * items.length)]
+    document.getElementById('goblinAmount').textContent = goblinAmount
+})
